bfd9637a5bc28c4aab30cea8e91b99b111ed9a12
Jihyeon Park <96460131+log2348@users.noreply.github.com>
2022-06-08 09:47:57 +0900

    Filter

--- filter_ex/src/main/java/com/example/filter_ex/filter/GlobalFilter.java
+++ filter_ex/src/main/java/com/example/filter_ex/filter/GlobalFilter.java
@@ -19,13 +19,13 @@ import lombok.extern.slf4j.Slf4j;
 
 // Servlet <-- Http 통신 확장한 클래스
 // 자바 언어로 웹 프로그래밍을 하기 위해 만들어진 클래스
-// 
 // Filter 기능을 활용하기 위해서는 구현 받아야 한다.
 
 // @Component // 메모리에 올려주는 녀석 ( new )
 // 로깅 처리를 많이 함
 // url 확인이라던지 특정 코드를 집어 넣어야 할 때 사용을 많이 한다.
-@Slf4j
+
+@Slf4j // System.out.println();이 아닌 log를 남길 수 있게 도와주는 lombok annotation
 @WebFilter(urlPatterns = "/api/*")
 public class GlobalFilter implements Filter {
 
@@ -33,17 +33,20 @@ public class GlobalFilter implements Filter {
 	// 스프링 중에 가장 핵심 개념 (IoC) Inversion of Control 제어의 역전
 	// 싱글톤 패턴으로 올라감, Dependency Injection (DI, 의존 주입, 필요한 시점에 객체를 가져와서 쓴다.)
 
-//	@Override
-//	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-//			throws IOException, ServletException {
-//		log.info("제일 먼저 request(요청)에 대해서 처리해보기");
-//		
-//		HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-//		log.info("req : {}", httpServletRequest.getProtocol());
-//		log.info("req : {}", httpServletRequest.getHeaderNames().toString());
-//		
-//		HttpServletResponse servletResponse = (HttpServletResponse) response;
-//		log.info("res : {}", servletResponse.getBufferSize());
+	/*
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		log.info("제일 먼저 request(요청)에 대해서 처리해보기");
+		
+		HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+		log.info("req : {}", httpServletRequest.getProtocol());
+		log.info("req : {}", httpServletRequest.getHeaderNames().toString());
+		
+		HttpServletResponse servletResponse = (HttpServletResponse) response;
+		log.info("res : {}", servletResponse.getBufferSize());
+	}
+	*/
 
 	// API Controller 못 가고 있음
 	// 기본적으로 한 번 읽은 request는 다시 읽을 수 없다 !!
